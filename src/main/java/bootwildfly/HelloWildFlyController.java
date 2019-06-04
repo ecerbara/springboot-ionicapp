105308621bacf6dd207ceb9bf0016094495b9142
EMEA\cerbara <emiliano.cerbara@dxc.com>
2019-06-04 12:13:17 +0200

    #0 fix pom

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -1,6 +1,7 @@
 package bootwildfly;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +12,38 @@ public class HelloWildFlyController {
     public String sayHello(){
         return ("Hello, SpringBoot on Wildfly");
     }
+    
+    @RequestMapping(value = "/helloIonic", method = RequestMethod.GET)
+	public Saluto testClient() {
+		System.out.println("entrati con app");
+	
+		return new Saluto("nome", "ciao Ionic");
+	}
+	
+	private class Saluto{
+		private String nome;
+		private String valore;
+		
+		public Saluto(String nome, String valore) {
+			super();
+			this.nome = nome;
+			this.valore = valore;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getValore() {
+			return valore;
+		}
+
+		public void setValore(String valore) {
+			this.valore = valore;
+		}
+	}
 }
